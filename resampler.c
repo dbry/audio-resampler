b94fe8bf487ccaf57c3f4b82cae04643a27aa52b
David Bryant <david@wavpack.com>
2023-07-21 18:50:37 -0700

    add detailed comments to describe the use of the resampler's global functions

--- resampler.c
+++ resampler.c
@@ -164,6 +164,64 @@ double subsample (Resample *cxt, float *source, double offset)
         return subsample_no_interpolate (cxt, source, offset);
 }
 
+// Initialize a resampler context with the specified characteristics. The returned context pointer
+// is used for all subsequent calls to the resampler (and should not be dereferenced). A NULL
+// return indicates an error. For the flags parameter, note that SUBSAMPLE_INTERPOLATE and
+// BLACKMAN_HARRIS are recommended for most applications. The parameters are:
+//
+// numChannels:     the number of audio channels present
+//
+// numTaps:         the number of taps for each sinc interpolation filter
+//                    - must be a multiple of 4, from 4 - 1024 taps
+//                    - affects quality by controlling cutoff sharpness of filters
+//                    - linearly affects memory usage and CPU load of resampling
+//
+// numFilters:      the number of sinc filters generated
+//                    - must be 2 - 1024
+//                    - affects quality of interpolated filtering
+//                    - linearly affects memory usage of resampler
+//
+// lowpassRatio:    enable lowpass by specifying ratio < 1.0 (relative to input samples);
+//                   this is required for downsampling, optional otherwise
+//
+// flags:           mask for optional feature configuration:
+//
+//   SUBSAMPLE_INTERPOLATE:     interpolate values from adjacent filters
+//                                - generally recommended except in special situations
+//                                - approximately doubles the CPU load
+//
+//   BLACKMAN_HARRIS:           use 4-term Blackman Harris window function
+//                                - generally recommended except in special situations
+//                                - if not specified, the default window is Hann (raised cosine)
+//                                  which has steeper cutoff but poorer stopband rejection
+//
+//   INCLUDE_LOWPASS:           include lowpass in sinc interpolation filters
+//                                - lowpassRatio specifies frequency as a ratio to input samples
+//                                - required for downsampling, optional otherwise
+//
+// Notes:
+//
+// 1. The same resampling instance can be used for upsampling, downsampling, or simple (near-unity)
+//    resampling (e.g., for asynchronous sample rate conversion, phase shifting, or filtering). The
+//    behavior is controlled by the "ratio" parameter during the actual resample processing call.
+//    To prevent aliasing, it's important to specify a lowpassRatio if the resampler will be used
+//    for any significant degree of downsampling (ratio < 1.0), but the lowpass can also be used
+//    independently without any rate conversion (or even upsampling).
+//
+// 2. When the context is initialized (or reset) the sample histories are filled with silence such
+//    that the resampler is ready to generate output immediately. However, this also means that there
+//    is an implicit signal delay equal to half the tap length of the sinc filters in samples. If
+//    zero delay is desired then that many samples can be ignored, or the resampleAdvancePosition()
+//    function can be used to bypass them. Also, at the end of processing an equal length of silence
+//    must be appended to the input audio to align the output with the actual input.
+//
+// 3. Both the number of interpolation filters and the number of taps per filter directly control
+//    the fidelity of the resampling. The filter length has an approximately linear affect on the
+//    the CPU load consumed by the resampler, and also on the memory requirement (both for the
+//    filters themselves and also for the sample history storage). On the other hand, the number
+//    of filters allocated primarily affects just the memory footprint (it has little affect on CPU
+//    load and so can be large on systems with lots of RAM).
+
 Resample *resampleInit (int numChannels, int numTaps, int numFilters, double lowpassRatio, int flags)
 {
     Resample *cxt = calloc (1, sizeof (Resample));
@@ -214,6 +272,9 @@ Resample *resampleInit (int numChannels, int numTaps, int numFilters, double low
     return cxt;
 }
 
+// Reset a resampler context to its initialized state. Specifically, any history is discarded
+// and this should be used when an audio "flush" or other discontinuity occurs.
+
 void resampleReset (Resample *cxt)
 {
     int i;
@@ -224,6 +285,19 @@ void resampleReset (Resample *cxt)
     cxt->outputOffset = cxt->numTaps / 2;
     cxt->inputIndex = cxt->numTaps;
 }
+
+// Run the resampler context at the specified output ratio and return both the number of input
+// samples consumed and output samples generated (in the ResampleResult structure). Over time
+// the average number of output samples will be equal to the number of input samples multiplied
+// by the given ratio, but of course in a single call only an integer number of samples can be
+// generated. The numInputFrames parameter indicates the number of samples available at "input"
+// and the numOutputFrames indicates the number of samples at "output" that can be written. The
+// resampling proceeds until EITHER the input is exhausted or space at the output is exhausted
+// (there is no other limit).
+//
+// This is the "non-interleaved" version of the resampler where the audio sample buffers for
+// different channels are passed in as an array of float pointers. There is also an
+// "interleaved" version (see below).
 
 ResampleResult resampleProcess (Resample *cxt, const float *const *input, int numInputFrames, float *const *output, int numOutputFrames, double ratio)
 {
@@ -264,6 +338,10 @@ ResampleResult resampleProcess (Resample *cxt, const float *const *input, int nu
     return res;
 }
 
+// This is the "interleaved" version of the resampler where the audio samples for different
+// channels are passed in sequence in a single buffer. There is also a "non-interleaved"
+// version for independent buffers, which is otherwise identical (see above).
+
 ResampleResult resampleProcessInterleaved (Resample *cxt, const float *input, int numInputFrames, float *output, int numOutputFrames, double ratio)
 {
     int half_taps = cxt->numTaps / 2, i;
@@ -302,6 +380,18 @@ ResampleResult resampleProcessInterleaved (Resample *cxt, const float *input, in
 
     return res;
 }
+
+// These two functions are not required for any application, but might be useful. Essentially
+// they allow a "dry run" of the resampler to determine beforehand how many input samples
+// would be consumed to generate a given output, or how many samples would be generated with
+// a given input.
+//
+// Note that there is a tricky edge-case here for ratios just over 1.0. If a query is made as
+// to how many input samples are required to generate a given output, that does NOT necessarily
+// mean that exactly that many samples will be generated with the indicated input (specifically
+// an extra sample might be generated). Therefore it is important to restrict the output with
+// numOutputFrames if an exact output count is desired (don't just assume the input count can
+// exactly determine the output count).
 
 unsigned int resampleGetRequiredSamples (Resample *cxt, int numOutputFrames, double ratio)
 {
@@ -359,6 +449,11 @@ unsigned int resampleGetExpectedOutput (Resample *cxt, int numInputFrames, doubl
     return res.output_generated;
 }
 
+// Advance the resampler output without generating any output, with the units referenced
+// to the input sample array. This can be used to temporally align the output to the input
+// (by specifying half the sinc filter tap width), and it can also be used to introduce a
+// phase shift. The resampler cannot be reversed.
+
 void resampleAdvancePosition (Resample *cxt, double delta)
 {
     if (delta < 0.0)
@@ -367,10 +462,41 @@ void resampleAdvancePosition (Resample *cxt, double delta)
         cxt->outputOffset += delta;
 }
 
+// Get the subsample position of the resampler. This is initialized to zero when the
+// resampler is started (or reset) and moves around zero as the resampler processes
+// audio. Obtaining this value is generally not required, but can be useful for
+// applications that need accurate phase information from the resampler such as
+// asynchronous sample rate converter (ASRC) implementations. The units are relative
+// to the input samples, and a negative value indicates that an output sample is
+// ready (i.e., can be generated with no further input read).
+//
+// To fully understand the meaning of the position value the following C-like
+// pseudo-code for the resampler is presented. Note that this code has the length
+// of the sinc filters and the actual interpolation abstracted away (like they
+// abstracted away from the user of the library):
+//
+// while (numOutputFrames > 0) {
+//     if (position < 0.0) {
+//         write (output);
+//         numOutputFrames--;
+//         position += (1.0 / ratio);
+//     }
+//     else if (numInputFrames > 0) {
+//         read (input);
+//         numInputFrames--;
+//         position -= 1.0;
+//     }
+//     else
+//         break;
+// }
+
 double resampleGetPosition (Resample *cxt)
 {
     return cxt->outputOffset + (cxt->numTaps / 2.0) - cxt->inputIndex;
 }
+
+// Free all resources associated with the resampler context, including the context pointer
+// itself. Do not use the context after this call.
 
 void resampleFree (Resample *cxt)
 {
